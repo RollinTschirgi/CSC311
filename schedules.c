1753c0d61cd2fddc33363011f28efcdd36d692cb
RollinTschirgi <rtschirgi16@cornellcollege.edu>
2015-11-23 14:28:12 -0600

    Create schedules.c

---
+++ schedules.c
@@ -0,0 +1,5 @@
+// "..the chief characteristic of the religion of science,(is) that it works.."
+                                                      -Issac Asimov, Foundation(1951)
+//Rollin Tschirgi
+//CSC311
+//23/Nov/2016
