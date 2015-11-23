590ac9eacb85eec3640823e133566e73ddc7d094
RollinTschirgi <rtschirgi16@cornellcollege.edu>
2015-11-23 13:28:45 -0600

    Update eratosthenes.c
    typed other half of code

--- eratosthenes.c
+++ eratosthenes.c
@@ -18,7 +18,7 @@ void filter(){
   int i = 0;
   int j = 2;
   
-  //initialize first two to 1 aka isPrime=true
+  //initialize first two to 1 aka: isDivisible=true
   bitmap[0] = bitmap[1] = 1;
   
   //initialize the rest to zero
@@ -29,8 +29,27 @@ void filter(){
   //begin search at 2 could be in the form of a four loop
   i=2;
   while(i < SIZE){
-  
-  
-  }
+    if(bitmap[i]==0){
+      
+      for(j=i+i; j<SIZE; j+=i){
+        bitmap[j]=1;
+      }//for
+      
+    }//if
+    i++;
+  }//while
 
-}
+}//filter()
+
+int main(int argc, char** argv){
+  
+  filter();
+  
+  int i;
+  for(i=0; i<SIZE; i++){
+    
+    if (bitmap[i] == 0){
+      printf("%d is prime \n", i);
+    }//if
+  }//for
+}//main
